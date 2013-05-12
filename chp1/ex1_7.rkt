fd4fc4feb4f8d9ed9c3be76bea8a68cfff610415
Nic Young <nryoung@gmail.com>
2013-05-12 00:52:52 -0600

    Finished implementing improved good-enough procedure. Exercise 1.7 complete. #1

--- chp1/ex1_7.rkt
+++ chp1/ex1_7.rkt
@@ -11,19 +11,19 @@
 ;;; procedure that uses this kind of end test. Does this work better for small
 ;;; and large numbers?
 
-(define (good-enough guess x)
-  (< (abs (- (square guess) x)) 0.001))
+;;; (define (good-enough guess x)
+;;; (< (abs (- (square guess) x)) 0.001))
 
 ;;; Answer:
 ;;; First we will take a look at relatively small numbers to see how
 ;;; good-enough fails
 
-(sqrt-iter 1 .5123)
+;;; (sqrt-iter 1 .5123)
 ;;; .7157521658272334
 ;;; Which is correct, but now if we add a significant amount of decimal places
 ;;; we can see it fail:
 
-(sqrt-iter 1 .00000000000000000005123)
+;;; (sqrt-iter 1 .00000000000000000005123)
 ;;; .03125
 ;;; Which is incorrect, the actual answer is some around 2.2634 * 10**-10
 ;;; On my machine giving Racket 128 MB there is only percision to the 23rd
@@ -31,11 +31,31 @@
 
 ;;; Now we take a look at it failing with a significantly large number:
 
-(sqrt-iter 1 99999999999999999)
+;;; (sqrt-iter 1 99999999999999999)
 ;;; No immediate result
 ;;; This may eventually produce a result, given that my initial guess is
 ;;; significantly lower then the actual square root, but who wants to wait
 ;;; around for that?
 
-;;; TODO: Design a better method for the good-enough procedure. Will have to
-;;; do more research on computing square roots.
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (sqrt-iter guess x)
+  (if (good-enough guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (good-enough guess x)
+  (< (abs (- guess (average guess (/ x guess)))) 0.001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))
+
+(sqrt 81)
