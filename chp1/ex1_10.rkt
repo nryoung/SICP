1e129a44f709b38d122c77b8dfc422354412ada1
Nic Young <nryoung@gmail.com>
2013-05-21 01:22:35 -0600

    Exercise 1.10 complete. #1

--- chp1/ex1_10.rkt
+++ chp1/ex1_10.rkt
@@ -10,6 +10,7 @@
                  (A x (- y 1))))))
 
 ;;; What are the values of the following expressions?
+;;; Answer:
 
 (A 1 10)
 ;;; 1024
@@ -22,14 +23,31 @@
 
 ;;; Consider the following procedures, where A is the procedure defined above:
 
-;;;(define (f n) (A 0 n))
+(define (f n) (A 0 n))
 
-;;;(define (g n) (A 1 n))
+(define (g n) (A 1 n))
 
-;;;(define (h n) (A 2 n))
+(define (h n) (A 2 n))
 
-;;;(define (k n) (* 5 n n))
+(define (k n) (* 5 n n))
 
 ;;; Give concise mathematical definitions for the functions computed by the 
 ;;; procedures f, g, and h for positive integer values of n. For example, 
 ;;; (k n) computes 5n**2. 
+
+;;; Answer:
+;;; (f n) computes 2n
+;;;
+;;; (g n) computes 2**n, this can be seen with the substitution model:
+;;; (A 1 5)
+;;; (A 0 (A 1 4))
+;;; (A 0 (A 0 (A 1 3)))
+;;; (A 0 (A 0 (A 0 (A 1 2))))
+;;; (A 0 (A 0 (A 0 (A 0 (A 1 1)))))
+;;; (A 0 (A 0 (A 0 (A 0 2))))
+;;; (A 0 (A 0 (A 0 4)))
+;;; (A 0 (A 0 8))
+;;; (A 0 16)
+;;; 32
+;;;
+;;; (h n) computes 2**2**2... (n times).
